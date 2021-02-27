c0dbb75d8fc326e169a41b6daf88a6a8853b01a8
Kevin Patel <kevinjaypatel@gmail.com>
2021-02-26 17:07:45 -0800

    rearranged navbar to display chrome extension link prior to learn more link

--- my-app/src/components/Navigation.js
+++ my-app/src/components/Navigation.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import logo from '../images/photosense-resized.png';
 import { Nav, Navbar } from 'react-bootstrap'; 
+import '../styles/navbar.css';
 
 const Navigation = () => (
     // Parent Navbar UI
@@ -10,24 +11,36 @@ const Navigation = () => (
         <Navbar.Brand>
             <img src={logo} alt="Photosense Logo" ></img>
         </Navbar.Brand>
-        {/* When in mobile view, Navbar.Toggle will 
-        render a hamburger menu to display nav options */}
+        {/* Renders a hamburger menu for mobile view */}
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         {/* UI of navbar when the screen size decreases 
         or when navbar collapses */}
         <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
-                <Link exact to="/">
+                <NavLink
+                    exact
+                    activeClassName="navbar__link--active"
+                    className="navbar__link"
+                    to="/"
+                >
                     Home
-                    </Link>
+                </NavLink>
 
-                <Link to="/learn-more">
-                    Learn More
-                    </Link>
-
-                <Link to="/dev">
+                <NavLink
+                    activeClassName="navbar__link--active"
+                    className="navbar__link"
+                    to="/dev"
+                >
                     Extension
-                </Link>
+                </NavLink>
+
+                <NavLink
+                    activeClassName="navbar__link--active"
+                    className="navbar__link"
+                    to="/learn-more"
+                >
+                    Learn More
+                </NavLink>
             </Nav>
         </Navbar.Collapse>
   </Navbar>
