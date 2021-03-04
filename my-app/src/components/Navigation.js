dfab88f9cf868c56d9ac3c8b54a27a6222c79c0b
Kevin Patel <kevinjaypatel@gmail.com>
2021-03-04 11:11:10 -0800

    added side bar navigation

--- my-app/src/components/Navigation.js
+++ my-app/src/components/Navigation.js
@@ -1,49 +1,181 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
-import logo from '../images/photosense-resized.png';
-import { Nav, Navbar } from 'react-bootstrap'; 
-import '../styles/navbar.css';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+//appbar
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import {Link as RouterLink} from 'react-router-dom';
+//drawer
+import Drawer from '@material-ui/core/Drawer';
+import Divider from '@material-ui/core/Divider';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import HomeRoundedIcon from '@material-ui/icons/HomeRounded';
+import DescriptionIcon from '@material-ui/icons/Description';
+import EditIcon from '@material-ui/icons/Edit';
+import Collapse from '@material-ui/core/Collapse';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ExtensionIcon from '@material-ui/icons/Extension';
+import InfoIcon from '@material-ui/icons/Info';
+//general
+import Typography from '@material-ui/core/Typography';
+import Hidden from '@material-ui/core/Hidden';
 
-const Navigation = () => (
-    // Parent Navbar UI
-    <Navbar collapseOnSelect="true" bg="light" expand="sm">
-        {/* Navbar Branding: PhotoSense Logo */}
-        <Navbar.Brand>
-            <img src={logo} alt="Photosense Logo" ></img>
-        </Navbar.Brand>
-        {/* Renders a hamburger menu for mobile view */}
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        {/* UI of navbar when the screen size decreases 
-        or when navbar collapses */}
-        <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto"> 
-                <NavLink
-                    exact
-                    activeClassName="navbar__link--active"
-                    className="navbar__link"
-                    to="/"
-                >
-                    Home
-                </NavLink>
+const drawerWidth = 240;
 
-                <NavLink
-                    activeClassName="navbar__link--active"
-                    className="navbar__link"
-                    to="/dev"
-                >
-                    Extension
-                </NavLink>
+const useStyles = makeStyles((theme) => ({
+  root : {
+    display: 'flex'
+  },
+  appBar : {
+    zIndex: theme.zIndex.drawer + 1,
+  },
+  menuButton : {
+    marginRight: theme
+  },
+  title: {
+    flexGrow: 1,
+  },
+  drawer : {
+    width : drawerWidth,
+    flexShrink: 0,
+  },
+  drawerMobile : {
+    overflowX: 'hidden',
+    width : theme.spacing(7) + 1,
+    [theme.breakpoints.up('sm')] : {
+      width: theme.spacing(9) + 1,
+    }
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+  toolbar: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+    ...theme.mixins.toolbar,
+  }
+}));
 
-                <NavLink
-                    activeClassName="navbar__link--active"
-                    className="navbar__link"
-                    to="/learn-more"
-                >
-                    Learn More
-                </NavLink>
-            </Nav>
-        </Navbar.Collapse>
-  </Navbar>
-);
+function Base(props) {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+  const mdDown = useMediaQuery(theme.breakpoints.down('md'));
+  const [collapseEncounter, setCollapseEncounter] = React.useState(false);
 
-export default Navigation;
+  const toggleCollapseEncounter = () => {
+    setCollapseEncounter(!collapseEncounter);
+    setOpen(false);
+  };
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  const closeButton = (
+    <div className={classes.toolbar}>
+      <IconButton onClick={handleDrawerClose}>
+        {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+      </IconButton>
+    </div>
+  );
+
+  const drawer = (
+    <div>
+      {mdDown ? closeButton : <div className={classes.toolbar} />}
+      <List component="nav">
+        <ListItem button component={RouterLink} to='/' onClick={handleDrawerClose}>
+          <ListItemIcon><HomeRoundedIcon /></ListItemIcon>
+          <ListItemText primary="Home" />
+        </ListItem>
+        <Divider />
+        <ListItem button component={RouterLink} to='/dev' onClick={handleDrawerClose}>
+          <ListItemIcon><ExtensionIcon /></ListItemIcon>
+          <ListItemText primary="Chrome Extension" />
+        </ListItem>
+        <Divider />
+        <ListItem button component={RouterLink} to='/learn-more' onClick={handleDrawerClose}>
+          <ListItemIcon><InfoIcon /></ListItemIcon>
+          <ListItemText primary="Learn More" />
+        </ListItem>
+        <Divider />
+      </List>
+    </div>
+  );
+  
+
+  return (
+    <div className={classes.root}>
+      <AppBar className={classes.appBar} position="fixed">
+        <Toolbar>
+          <Hidden lgUp>
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={handleDrawerOpen}>
+              <MenuIcon />
+            </IconButton>
+          </Hidden>
+          <Typography variant="h5" className={classes.title}>
+            PhotoSense
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <Hidden mdDown>
+        <Drawer
+          className={classes.drawer}
+          variant="permanent"
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+        >
+        { drawer }
+        </Drawer>
+      </Hidden>
+
+      <Hidden lgUp>
+        <Drawer
+          className={classes.drawerMobile}
+          variant="permanent"
+          classes={{
+            paper: classes.drawerMobile,
+          }}
+        >
+          {drawer}
+        </Drawer>
+        <Drawer
+          className={classes.drawer}
+          classes={{
+            paper: classes.drawer,
+          }}
+          anchor="left"
+          open={open}
+        >
+          {drawer}
+        </Drawer>
+      </Hidden>
+
+      <main className={classes.content}>
+        <div className={classes.toolbar}/>
+      </main>
+
+    </div>
+  )
+}
+
+export default Base
