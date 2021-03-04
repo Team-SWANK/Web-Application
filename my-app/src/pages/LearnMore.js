b4f5ce479ed66e19117286db2f987fd1f4cb5a57
Kevin Patel <kevinjaypatel@gmail.com>
2021-03-04 11:59:13 -0800

    using material container to horizontally center the learn more content

--- my-app/src/pages/LearnMore.js
+++ my-app/src/pages/LearnMore.js
@@ -1,28 +1,36 @@
 import React from 'react'; 
-import '../styles/learn-more.css';
-import {Container, Row, Col} from 'react-bootstrap'; 
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+// general
+import Container from '@material-ui/core/Container'; 
 
-function LearnMore() {
+const useStyles = makeStyles((theme) => ({
+    content: {
+        // flexGrow: 1, 
+        padding: theme.spacing(3),
+        border: '5px solid #eceff1', 
+    }, 
+})); 
+    
+function LearnMore(props) {
+    const classes = useStyles(); 
+    const theme = useTheme(); 
     
     return (
         <Container>
-            <Row>
-                <Col>
-                    <div className="learnMoreBody">
-                        <h2>About PhotoSense</h2>
-                        <p className="infoText">PhotoSense is a set of online tools that give people the ability to protect their privacy. 
-                        In 2020, many activists and journalists are using the internet to upload images of protests and 
-                        other politically charged mass gatherings. <br></br> <br></br> However, those who upload their images online may be susceptible to 
-                        threats, job loss, and physical assault. PhotoSense addresses this issue by providing twitter and reddit bots, a 
-                        browser extension, and a web application for users to edit and upload censored images online. <br></br> <br></br> PhotoSense is not
-                        only an application, it is a movement. We strive to emphasize the invaluable rights to peacefully gather, while maintaining the the security
-                        of privacy. In order to further this movement, PhotoSense provides its users with a Google Chrome Extension, as well as Reddit and Twitter bots.
-                        The Google Chrome Extension can be accessed through the Google app store, or by clicking the link in the top navigation bar. The Reddit and Twitter social media
-                        bots can be utilized by anyone who wants to spread this movement by using the appropriate hashtags (#PhotoSense) and commands.         
-                        </p>
-                    </div>
-                </Col>
-            </Row>
+            <div className={classes.content}>
+                <h2>About PhotoSense</h2>
+                <p>PhotoSense is a set of online tools that give people the ability to protect their privacy. 
+                In 2020, many activists and journalists are using the internet to upload images of protests and 
+                other politically charged mass gatherings. <br></br> <br></br> However, those who upload their images online may be susceptible to 
+                threats, job loss, and physical assault. PhotoSense addresses this issue by providing twitter and reddit bots, a 
+                browser extension, and a web application for users to edit and upload censored images online. <br></br> <br></br> PhotoSense is not
+                only an application, it is a movement. We strive to emphasize the invaluable rights to peacefully gather, while maintaining the security
+                of privacy. To further this movement, PhotoSense provides users with a Google Chrome Extension, as well as Reddit and Twitter bots.
+                The Google Chrome Extension can be accessed through the Google app store, or by clicking the link in the top navigation bar. The Reddit and Twitter social media
+                bots can be utilized by anyone who wants to spread the movement by using the appropriate hashtags (#PhotoSense) and commands.         
+                </p>
+            </div>
         </Container>
     ); 
 }
