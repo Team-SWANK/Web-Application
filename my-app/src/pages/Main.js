412be4869a805b56f8c5d2be0a53bc5b8eb90741
Kevin Patel <kevinjaypatel@gmail.com>
2021-04-14 10:46:15 -0700

    added post request for getting image masks from the segmentation api

--- my-app/src/pages/Main.js
+++ my-app/src/pages/Main.js
@@ -1,11 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import clsx from 'clsx';
-import Dropzone from 'react-dropzone';
+import Dropzone, { useDropzone } from 'react-dropzone';
 import CanvasPagination from '../components/CanvasPagination';
 import Container from '@material-ui/core/Container';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import FormData from 'form-data';
+import { resizeImage } from '../utils/utils';
+
+const axios = require('axios');
 
 const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    '& > * + *': {
+      marginTop: theme.spacing(2),
+    },
+  },
   center: {
     display: 'block',
     marginLeft: 'auto',
@@ -32,36 +43,84 @@ const useStyles = makeStyles((theme) => ({
 function Main() {
   const classes = useStyles();
   const [images, setImages] = useState([]);
-
-  const onDrop = acceptedFiles => {
-    console.log(acceptedFiles)
+  const [imagesSegmented, setImagesSegmented] = useState(false); 
+  const [imagesUploaded, setImageUploaded] = useState(false);
+  const [imageMasks, setImageMasks] = useState([]);   
+  
+  const onDrop = async (acceptedFiles) => {  
+    // render progress indicator after image is uploaded
+    setImageUploaded(true); 
     setImages(acceptedFiles.map(file => Object.assign(file, {
       preview: URL.createObjectURL(file)
     })));
+
+    console.log(acceptedFiles);
+
+    await getImageMasksAsync(acceptedFiles);  
   };
+
+  async function getImageMasksAsync(acceptedFiles) { 
+    let masks = [];
+    await acceptedFiles.forEach(async (image, i) => {
+      // resize the image before calling the segmentation api  
+      let resizedImage = await resizeImage(image);
+      // console.log(await resizedImage.text());
+      // make the POST request
+      let form = new FormData(); 
+      form.append('image', resizedImage, resizedImage.fileName);
+      let response = await axios({
+        method: "post",
+        url: "http://13.57.50.47:5000/",
+        data: form,
+        headers: { 'Content-Type': `multipart/form-data; boundary=${form._boundary}`, },
+      })
+      masks.push(response.data.predictions);
+     
+      // after all images have been masked 
+      // set image masks, and segmentation true 
+      // so CanvasPagination can be rendered with 
+      // the props set   
+      if(i === acceptedFiles.length - 1) {   
+        setImageMasks(masks); 
+        setImagesSegmented(true); 
+        console.log('total masks: ' + masks.length);  
+      }
+    });
+  }
 
   useEffect(() => () => {
     // Make sure to revoke the data uris to avoid memory leaks
-    images.forEach(file => URL.revokeObjectURL(file.preview));
+    images.forEach((image) => URL.revokeObjectURL(image.preview));  
   }, [images]);
 
-  return (
-    <Container>
-      {images.length > 0
-        ? <CanvasPagination images={images} />
-        : <Dropzone onDrop={onDrop}>
-          {({ getRootProps, getInputProps }) => (
-            <section>
-              <div {...getRootProps()} className={clsx(classes.center, classes.dropOutline)}>
-                <input {...getInputProps()} />
-                <p style={{ textAlign: 'center' }}>Drag 'n' drop some files here, or click to select files</p>
-              </div>
-            </section>
-          )}
+  if(images.length > 0 && imagesSegmented) {
+    return (
+      <Container>
+        <CanvasPagination images={images} imageMasks={imageMasks} />
+      </Container>
+    ); 
+  } else if(imagesUploaded) {
+    return(
+      <Container className={classes.root}>
+        <LinearProgress />
+      </Container>
+    );  
+  } else {
+    return(
+      <Container>
+          <Dropzone onDrop={onDrop} accept="image/*">
+            {({ getRootProps, getInputProps, isDragReject }) => (
+              <section>
+                <div {...getRootProps()} className={clsx(classes.center, classes.dropOutline)}>
+                  <input {...getInputProps()} />
+                  <p style={{ textAlign: 'center' }}>Drag 'n' drop some files here, or click to select files</p>
+                </div>
+              </section>
+            )}
         </Dropzone>
-      }
-    </Container>
-  );
+      </Container>
+    ); 
+  }
 }
 
 export default Main;
