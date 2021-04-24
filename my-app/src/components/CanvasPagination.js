5cb89957cf0ff2925ad7db3fc013461dada00c58
Kevin Patel <kevinjaypatel@gmail.com>
2021-04-23 18:26:04 -0700

    update for CanvasPagintation (still in the works)

--- my-app/src/components/CanvasPagination.js
+++ my-app/src/components/CanvasPagination.js
@@ -1,14 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router';
-import { makeStyles } from '@material-ui/core/styles';
+import { useTheme, makeStyles } from '@material-ui/core/styles';
 import Pagination from '@material-ui/lab/Pagination';
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
 import Button from '@material-ui/core/Button';
 import Canvas from '../Canvas';
 import CensorshipOptionsDialog from "./CensorshipOptionsDialog.js";
-import { getDimensions } from '../utils/utils'; 
 import LinearProgress from '@material-ui/core/LinearProgress';
+import CensorshipForm from './CensorshipForm.js'; 
+// Censorship Options Dialog
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+// Form Components 
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+// checkbox
+import Checkbox from '@material-ui/core/Checkbox';
+// switch 
+import Switch from '@material-ui/core/Switch';
+import { convertMask2dToImage, resizeImage } from '../utils/utils';
 const axios = require('axios');
 
 const useStyles = makeStyles((theme) => ({
@@ -47,11 +62,25 @@ const useStyles = makeStyles((theme) => ({
     color: "#181818",
     backgroundColor: "#dbdbdb",
     marginRight: "10px"
+  }, 
+  toolbarButton: {
+    width: "155px",
+    fontWeight: "bold",
+    color: "#181818",
+    backgroundColor: "#eceff1",
+  },
+  formControl: {
+    margin: theme.spacing(3),
+  }, 
+  formComponents: {
+    display: 'flex',
   }
+
 }));
 
-function CanvasPagination({ images, imageMasks }) {
+function CanvasPagination({ images, imageMasks, resizedImages }) {
   const classes = useStyles();
+  const theme  = useTheme(); 
   const history = useHistory();
 
   const [page, setPage] = useState(1);
@@ -59,21 +88,32 @@ function CanvasPagination({ images, imageMasks }) {
   const [isCensored, setIsCensored] = useState(false);
   const [isCensoring, setIsCensoring] = useState(false); 
 
-  const [censoringAlgorithms, setCensoringAlgorithms] = useState([
-    { name: 'pixel_sort', isSelected: false, label: 'Pixel Sort', id: 1 }, 
-    { name: 'simple_blurring', isSelected: false, label: 'Simple Blurring', id: 2 },
-    { name: 'pixelization', isSelected: false, label: 'Pixelization', id: 3 }, 
-    { name: 'black_bar', isSelected: false, label: 'Black Bar', id: 4 },
-    { name: 'fill_in', isSelected: false, label: 'Fill In', id: 5 },
-  ]); 
+  const [openDialog, setOpenDialog] = useState(false); 
+  const fullScreen = useMediaQuery(theme.breakpoints.down('sm')); 
 
-  // const { pixel_sort, simple_blurring, pixelization, black_bar, fill_in } = checkBoxState;
+  const [checkBoxState, setCheckboxState] = React.useState({
+    pixel_sort: false,
+    simple_blurring: false,
+    pixelization: false,
+    black_bar: false,
+    fill_in: false,
+  });
 
-  let url = 'http://127.0.0.1:5001/api/censor?options='; 
+  const { pixel_sort, simple_blurring, pixelization, black_bar, fill_in } = checkBoxState;
 
-  const handleCensoringAlgorithmsChange = (event) => {
-    setCensoringAlgorithms({...censoringAlgorithms, [event.target.name]: event.target.checked});
-    console.log(censoringAlgorithms);  
+  let url = 'http://18.144.37.100:8000/Censor?options=[pixel_sort]'; 
+  let censoredImage; 
+
+  const handleCheckboxChange = (event) => {
+    setCheckboxState({ ...checkBoxState, [event.target.name]: event.target.checked }); 
+  };
+
+  const handleOpenDialog = () => {
+    setOpenDialog(true); 
+  }
+
+  const handleCloseDialog = () => {
+    setOpenDialog(false); 
   }
 
   const handlePagination = (event, value) => { 
@@ -87,10 +127,49 @@ function CanvasPagination({ images, imageMasks }) {
   }
 
   const censorImages = async () => {
-    setIsCensoring(true); 
-    // call api and retreive censored images
-    let form = new FormData(); 
-    
+    let currentPage = page - 1;  
+    // let values = Object.values(checkBoxState);  
+    // if(!values.includes(true)) {
+    //   alert('No censoring algorithm were chosen');  
+    // }
+    //  else {
+      setIsCensoring(true); 
+      let imageMask2dArray = coordsPass[currentPage]; 
+      let maskedImage = await convertMask2dToImage(imageMask2dArray, currentPage);
+      censoredImage = await getCensoredImageAsync(maskedImage, currentPage); 
+      setIsCensored(true); 
+      setIsCensoring(false); 
+    // }
+  }  
+
+  async function getCensoredImageAsync(maskedImage, currentPage) {
+    let response; 
+    let resizedImage = resizedImages[currentPage]; 
+    // call api and retreive censored image
+    let form = new FormData();
+    form.append('image', resizedImage, resizedImage.fileName); 
+    form.append('mask', maskedImage, maskedImage.fileName);
+    // api response returns the image encoded in base64
+    try {
+      response = axios({
+        method: 'post',
+        url: url,
+        data: form, 
+        headers: { 'Content-Type': `multipart/form-data; boundary=${form._boundary}`, },
+      }).then(response => {
+        return 'data:image/jpeg;base64,' + response.data.ImageBytes;
+      });
+    } catch(err) {
+      console.log('error detected'); 
+    }
+    response = await Promise.resolve(response);
+    // console.log(response);  
+    let image = new Image();
+    image.onload = () => {
+      console.log(image);
+      return image; 
+    } 
+    image.src = response; 
   }
 
   const download = () => {
@@ -122,9 +201,12 @@ function CanvasPagination({ images, imageMasks }) {
   
 
   useEffect(() => {    
-    setCoordsPass(imageMasks);
-    console.log(imageMasks.length); 
+    setCoordsPass(imageMasks); 
   }, [imageMasks]);
+
+  useEffect(() => {
+    console.log(resizedImages); 
+  }, [resizedImages]);
 
   if(isCensoring) {
     return(
@@ -146,26 +228,81 @@ function CanvasPagination({ images, imageMasks }) {
                   Download
                 </Button>
               </Grid>
+              {/* Canvas Component */}
+              <Canvas
+                image={censoredImage}
+                coordsPass={coordsPass[page - 1]}
+                setCoordsPass={handleCoordsChange}
+              />
             </Grid>
           : <Grid container>
               <Grid item xs={6}>
                 <Button size='small' className={classes.censorButton} onClick={censorImages}>
                   Censor
                 </Button>
-                <CensorshipOptionsDialog 
-                  options={censoringAlgorithms} 
-                  handleOptionsChange={handleCensoringAlgorithmsChange}  
-                />
+                <Button size='small' className={classes.toolbarButton} onClick={handleOpenDialog}>
+                  Select Options
+                </Button>
+                {/* Censor Options Dialog */}
+                <Dialog fullScreen={fullScreen} open={openDialog} onClose={handleCloseDialog}
+                  aria-labelledby="responsive-dialog-title"
+                >
+                  <DialogContent>
+                    {/* <CensorshipForm options={censoringAlgorithms} handleOptionsChange={handleCensoringAlgorithmsChange}/> */}
+                    <div className={classes.formComponents}>
+                    {/* Checkbox Components */}
+                    <FormControl component="fieldset" className={classes.formControl}>
+                      <FormLabel component="legend">Select Censoring Algorithm(s)</FormLabel>
+                      <FormGroup>
+                        <FormControlLabel
+                          control={<Checkbox checked={pixel_sort} onChange={handleCheckboxChange} name="pixel_sort" />}
+                          label="Pixel Sorting"
+                        />
+                        <FormControlLabel
+                          control={<Checkbox checked={simple_blurring} onChange={handleCheckboxChange} name="simple_blurring" />}
+                          label="Simple Blurring"
+                        />
+                        <FormControlLabel
+                          control={<Checkbox checked={pixelization} onChange={handleCheckboxChange} name="pixelization" />}
+                          label="Pixelization"
+                        />
+                        <FormControlLabel
+                          control={<Checkbox checked={black_bar} onChange={handleCheckboxChange} name="black_bar" />}
+                          label="Black Bar Censoring"
+                        />
+                        <FormControlLabel
+                          control={<Checkbox checked={fill_in} onChange={handleCheckboxChange} name="fill_in" />}
+                          label="Fill In Censoring"
+                        />
+                      </FormGroup>
+                    </FormControl>
+                    {/* Switch Component */}
+                    {/* <FormControl component='fieldset' className={classes.formControl}>
+                      <FormLabel component="legend">Enable Metadata Scrubbing</FormLabel>
+                          <Switch
+                              checked={enableMeta}
+                              onChange={handleMetaChange}
+                              name="enableMeta"
+                              inputProps={{ 'aria-label': 'secondary checkbox' }}
+                          />
+                    </FormControl> */}
+                  </div>
+                  </DialogContent>
+                  <DialogActions>
+                    <Button autoFocus onClick={handleCloseDialog} color="primary">
+                      Close
+                    </Button>
+                  </DialogActions>
+                </Dialog>
               </Grid>
-            </Grid>
+              {/* Canvas Component */}
+              <Canvas
+                image={[images[page - 1]]}
+                coordsPass={coordsPass[page - 1]}
+                setCoordsPass={handleCoordsChange}
+              />
+          </Grid>
         }
-        
-        {/* Canvas Component */}
-        <Canvas
-          image={[images[page - 1]]}
-          coordsPass={coordsPass[page - 1]}
-          setCoordsPass={handleCoordsChange}
-        />
         {/* Pagination Component */}
         <Grid container justify="center">
           {images.length > 1
